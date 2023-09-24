26d5a2558a8aa80cdde4a2041409d0dc6f8cabc7
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2023-09-24 16:13:06 +0800

    Added meta and misc improvements.

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
+        <meta charset="<?php bloginfo("charset") ?>">
+        <meta name="viewport" content="width=device-width", initial-scale=1>
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
         <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
@@ -14,11 +16,11 @@
             <div class="site-header__menu group">
             <nav class="main-navigation">
                 <ul>
-                <li><a href="<?php echo site_url("/about-us"); ?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-                <li><a href="#">Blog</a></li>
+                    <li <?php echo is_page('about-us') || wp_get_post_parent_id(0) == 12 ? "class='current-menu-item'" : "" ?>><a href="<?php echo site_url("/about-us"); ?>">About Us</a></li>
+                    <li><a href="#">Programs</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Campuses</a></li>
+                    <li><a href="#">Blog</a></li>
                 </ul>
             </nav>
             <div class="site-header__util">
