ee82ca6ec03bfb5d3559b43b130dda7499efcfbb
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2023-10-01 19:16:58 +0800

    Added professors post type.

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -43,6 +43,23 @@ function university_post_types() {
         ],
         "menu_icon" => "dashicons-awards"
     ]);
+
+    // Professor Post Type
+    register_post_type("professor", [
+        "supports" => [
+            "title", "editor"
+        ],
+        "public" => true,
+        "show_in_rest" => true,
+        "labels" => [
+            "name" => "Professors",
+            "add_new_item" => "Add New Professor",
+            "edit_item" => "Edit Professor",
+            "all_items" => "All Professors",
+            "singular_name" => "Professor"
+        ],
+        "menu_icon" => "dashicons-welcome-learn-more"
+    ]);
 }
 
 add_action("init", "university_post_types");
---
+++ wp-content/themes/wp-university-theme/single-professor.php
@@ -0,0 +1,36 @@
+<?php 
+    get_header();
+    while(have_posts()) {
+        the_post(); ?>
+        
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>)"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                <p>Don't forget to replace me later.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container container--narrow page-section">
+            <div class="generic-content">
+                <?php the_content(); ?>
+                <?php 
+                    $relatedPrograms = get_field("related_programs");
+                    if($relatedPrograms) { ?>
+                        <hr class="section-break"/>
+                        <h2 class="headline headline--medium">Subject(s) Taught</h2>
+                        <ul class="link-list min-list">
+                        <?php foreach($relatedPrograms as $program){ ?>
+                            <li>
+                                <a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a>
+                            </li>
+                        <?php } ?>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    <?php }
+    get_footer();
+?>
