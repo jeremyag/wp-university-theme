7f346d00e3dcd8b85a90e42ce6af065848f6c3f3
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2023-10-22 19:56:35 +0800

    Added campuses page.

--- wp-content/themes/wp-university-theme/single-campus.php
+++ wp-content/themes/wp-university-theme/single-campus.php
@@ -7,7 +7,7 @@
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("program"); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> 
+                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("campus"); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Campuses</a> 
                     <span class="metabox__main"><?php the_title(); ?></span>
                 </p>
             </div>
@@ -18,32 +18,29 @@
             <?php 
                 $today = date("Ymd");
 
-                $relatedProfessors = new WP_Query([
+                $relatedPrograms = new WP_Query([
                     "posts_per_page" => -1,
-                    "post_type" => "professor",
+                    "post_type" => "program",
                     "orderby" => "title",
                     "order" => "ASC",
                     "meta_query" => [
                         [
-                            "key" => "related_programs",
+                            "key" => "related_campus",
                             "compare" => "LIKE",
                             "value" => '"'.get_the_ID().'"'
                         ]
                     ]
                 ]);
 
-                if($relatedProfessors->have_posts()){ ?>
+                if($relatedPrograms->have_posts()){ ?>
                     <hr class="section-break"/>
-                    <h2 class="headline headline--medium"><?php echo get_the_title(); ?> Professors</h2>
-                    <ul>
+                    <h2 class="headline headline--medium">Programs Available At This Campus</h2>
+                    <ul class="min-list link-list">
                     <?php
-                        while($relatedProfessors->have_posts()){
-                            $relatedProfessors->the_post(); ?>
-                            <li class="professor-card__list-item">
-                                <a class="professor-card" href="<?php the_permalink(); ?>">
-                                    <img class="professor-card__image" src="<?php the_post_thumbnail_url("professorLandscape"); ?>"/>
-                                    <span class="professor-card__name"><?php the_title(); ?></span>
-                                </a>
+                        while($relatedPrograms->have_posts()){
+                            $relatedPrograms->the_post(); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                             </li>
                         <?php } wp_reset_postdata(); ?>
                     </ul>
@@ -77,23 +74,10 @@
                         while($homePageEvents->have_posts()){
                             $homePageEvents->the_post(); 
                             get_template_part("template-parts/content", "event");
-                        } 
+                        }
+                        wp_reset_postdata();
                 }
-                wp_reset_postdata();  
-
-                $relatedCampuses = get_field("related_campus");
-
-                if($relatedCampuses){ ?>
-                        <hr class="section-break"/> 
-                        <h2 class="headline headline--medium"><?php  the_title(); ?> is Available At These Campuses:</h2>
-                        <ul class="min-list link-list">
-                            <?php foreach($relatedCampuses as $campus) { ?>
-                                <li>
-                                    <a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                <?php } ?>
+            ?>
         </div>
     <?php }
     get_footer();
